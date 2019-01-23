f1a5adb5fee4a17b02c8d67accfcd3137bedbccf
Michael Goodnow <mmgoodnow@gmail.com>
2019-01-23 16:31:13 -0500

    Implement findUserById() for User service

--- src/main/java/edu/neu/cs4500/services/UserService.java
+++ src/main/java/edu/neu/cs4500/services/UserService.java
@@ -1,6 +1,8 @@
 package edu.neu.cs4500.services;
 
 import edu.neu.cs4500.models.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -16,5 +18,21 @@ public class UserService {
 	static {
 		users.add(new User(123, "alice", "alice", "Alice", "Wonderland"));
 		users.add(new User(234, "bob", "bob", "Bob", "Marley"));
+	}
+
+	@GetMapping("/api/user")
+	public List<User> findAllUsers() {
+		return users;
+	}
+
+	@GetMapping("/api/user/{userId}")
+	public User findUserById(
+			@PathVariable("userId") Integer id) {
+		for (User user : users) {
+			if (user.getId().equals(id)) {
+				return user;
+			}
+		}
+		return null;
 	}
 }
