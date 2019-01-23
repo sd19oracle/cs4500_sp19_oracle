047974141ae5cdba4a04c0598546e87de39a9db7
Michael Goodnow <mmgoodnow@gmail.com>
2019-01-23 16:20:55 -0500

    Implement findAllUsers() in User Service

--- src/main/java/edu/neu/cs4500/services/UserService.java
+++ src/main/java/edu/neu/cs4500/services/UserService.java
@@ -1,8 +1,20 @@
 package edu.neu.cs4500.services;
 
+import edu.neu.cs4500.models.User;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Michael Goodnow on 2019-01-23.
  */
 
+@RestController
 public class UserService {
+	static List<User> users = new ArrayList<>();
+	static {
+		users.add(new User(123, "alice", "alice", "Alice", "Wonderland"));
+		users.add(new User(234, "bob", "bob", "Bob", "Marley"));
+	}
 }
