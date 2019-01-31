ea8b231568f18f954d633eff39aa2e29ec021b1e
DM-christiangomez <christiangomez@desktopmetal.com>
2019-01-31 12:57:35 -0500

    Implemented Service Repo Interface

--- src/main/java/edu/neu/cs4500/repositories/ServiceRepository.java
+++ src/main/java/edu/neu/cs4500/repositories/ServiceRepository.java
@@ -1,5 +1,16 @@
 package edu.neu.cs4500.repositories;
 
-public class ServiceRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import edu.neu.cs4500.models.Service;
+
+public interface ServiceRepository extends CrudRepository<Service, Integer> {
+	@Query(value="SELECT service FROM Service service")
+	public List<Service> findAllServices();
+	@Query(value="SELECT service FROM Service service WHERE service.id=:id")
+	public Service findServiceById(@Param("id") Integer id);
 }
