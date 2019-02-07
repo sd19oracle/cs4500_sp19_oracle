484da506757f097596d65bd77e1d5913eb66cacd
cvan cao <cvanzy@cvans-MacBook-Air.local>
2019-02-07 16:17:47 -0500

    initialize service specific answer and question reporistories

---
+++ src/main/java/edu/neu/cs4500/repositories/ServiceSpecificQuestion.java
@@ -0,0 +1,8 @@
+package edu.neu.cs4500.repositories;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface ServiceSpecificQuestion {
+}
