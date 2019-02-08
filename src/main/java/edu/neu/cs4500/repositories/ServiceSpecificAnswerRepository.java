d2f3be7f574e27647afadc63cbea75ce1068462c
Zerun <tian.ze@husky.neu.edu>
2019-02-07 21:03:38 -0500

    add service specific answer repository

---
+++ src/main/java/edu/neu/cs4500/repositories/ServiceSpecificAnswerRepository.java
@@ -0,0 +1,19 @@
+package edu.neu.cs4500.repositories;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+import edu.neu.cs4500.models.ServiceSpecificAnswer;
+import edu.neu.cs4500.models.User;
+
+public interface ServiceSpecificAnswerRepository extends CrudRepository<User, Integer> {
+  @Query(value = "SELECT serviceSpecificAnswer FROM ServiceSpecificAnswer serviceSpecificAnswer")
+  public List<ServiceSpecificAnswer> findAllServiceSpecificAnswers();
+
+  @Query(value = "SELECT serviceSpecificAnswer FROM ServiceSpecificAnswer serviceSpecificAnswer "
+          + "WHERE serviceSpecificAnswer.id=:id")
+  public ServiceSpecificAnswer findServiceSpecificAnswerById(@Param("id") Integer id);
+}
