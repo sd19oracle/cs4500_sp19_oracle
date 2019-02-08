0af4b8e4e1c3d50face56fbe34357bce5e83fcf6
cvanzy <cao.zhi@husky.neu.edu>
2019-02-07 20:21:58 -0500

    add insert data for service specific questions

--- src/main/java/edu/neu/cs4500/repositories/ServiceSpecificAnswerRepository.java
+++ src/main/java/edu/neu/cs4500/repositories/ServiceSpecificAnswerRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-public interface ServiceSpecificAnswer {
+public interface ServiceSpecificAnswerRepository {
+
 }
--- src/main/java/edu/neu/cs4500/repositories/ServiceSpecificQuestionRepository.java
+++ src/main/java/edu/neu/cs4500/repositories/ServiceSpecificQuestionRepository.java
@@ -1,9 +1,19 @@
 package edu.neu.cs4500.repositories;
 
+import edu.neu.cs4500.models.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import edu.neu.cs4500.models.ServiceSpecificQuestion;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 
-public interface ServiceSpecificQuestion extends CrudRepository<> {
+public interface ServiceSpecificQuestionRepository extends CrudRepository<ServiceSpecificQuestion, Integer> {
+    @Query(value="SELECT servicespecificquestion FROM User user")
+    public List<User> findAllServiceSpecificQuestion();
+    @Query(value="SELECT user FROM User user WHERE user.id=:id")
+    public User findUserById(@Param("id") Integer id);
+    @Query(value="SELECT user FROM User user WHERE user.username=:username")
+    public User findByUsername(@Param("username") String username);
 }
