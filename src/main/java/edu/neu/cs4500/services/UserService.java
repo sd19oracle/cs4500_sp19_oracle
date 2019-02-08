8cb78161deb1de7a317920c95a466f209a72934a
wolf-zhang <zhang.li@husky.neu.edu>
2019-02-08 15:25:49 -0500

    Assignment-3-resubmit

--- src/main/java/edu/neu/cs4500/services/UserService.java
+++ src/main/java/edu/neu/cs4500/services/UserService.java
@@ -1,78 +1,47 @@
-package edu.neu.cs4500.services;
+package com.example.cs4500sp19s3jga.services;
 
-import edu.neu.cs4500.models.User;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import edu.neu.cs4500.repositories.UserRepository;
-
-
-import java.util.List;
-
-/**
- * Created by Michael Goodnow on 2019-01-23.
- */
+import com.example.cs4500sp19s3jga.models.User;
+import com.example.cs4500sp19s3jga.repositories.UserRepository;
 
 @RestController
 public class UserService {
 	@Autowired
 	UserRepository userRepository;
-
-
-
-	@GetMapping("/api/user")
-	public List<User> findAllUsers() {
-
+	@GetMapping("/api/users")
+	public List<User> findAllUser() {
 		return userRepository.findAllUsers();
 	}
-
-	@GetMapping("/api/user/{userId}")
+	@GetMapping("/api/users/{userId}")
 	public User findUserById(
 			@PathVariable("userId") Integer id) {
 		return userRepository.findUserById(id);
 	}
-
-	@GetMapping("api/user/{username}")
-	public User findUserByName(
-					@PathVariable("username") String name)
-	{
-		return userRepository.findByUsername(name);
+	@PostMapping("/api/users")
+	public User createUser(@RequestBody User user) {
+		return userRepository.save(user);
 	}
-
-	@PostMapping("api/user")
-	public User createUser(
-					@RequestBody User newUser
-	) {
-		return userRepository.save(newUser);
-	}
-
-	@PutMapping("api/user/{userId}")
+	@PutMapping("/api/users/{userId}")
 	public User updateUser(
-					@PathVariable("userId") Integer id,
-					@RequestBody User updateUser
-	)
-	{
-		User find = userRepository.findUserById(id);
-		find.setId(updateUser.getId());
-		find.setAnswers(updateUser.getAnswers());
-		find.setFirstName(updateUser.getFirstName());
-		find.setLastName(updateUser.getLastName());
-		find.setPassword(updateUser.getPassword());
-		find.setRole(updateUser.getRole());
-		find.setUsername(updateUser.getUsername());
-		return find;
+			@PathVariable("userId") Integer id,
+			@RequestBody User userUpdates) {
+		User user = userRepository.findUserById(id);
+		user.setRole(userUpdates.getRole());
+		return userRepository.save(user);
 	}
-
-	@DeleteMapping("api/user/{userId}")
-	public void removeAUser(
-					@PathVariable("userID") Integer id
-	) {
+	@DeleteMapping("/api/users/{userId}")
+	public void deleteUser(
+			@PathVariable("userId") Integer id) {
 		userRepository.deleteById(id);
 	}
 }
