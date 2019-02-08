9890097faa492e10766bc0664e6f4bdb2a6e817d
Jiahao Cai <cai.jiah@husky.neu.edu>
2019-02-07 23:18:57 -0500

    insert data for providers' answers

---
+++ src/main/resources/sql/insert_service_specific_answers.sql
@@ -0,0 +1,7 @@
+INSERT INTO `oracle`.`service_specific_answer` (`id`, `answer`, `question_id`, `user_id`) VALUES (1, '10', 1, 345);
+INSERT INTO `oracle`.`service_specific_answer` (`id`, `answer`, `question_id`, `user_id`) VALUES (2, '11', 2, 456);
+INSERT INTO `oracle`.`service_specific_answer` (`id`, `answer`, `question_id`, `user_id`) VALUES (3, 'Monday through Friday', 3, 345);
+INSERT INTO `oracle`.`service_specific_answer` (`id`, `answer`, `question_id`, `user_id`) VALUES (4, 'All days in week', 4, 456);
+INSERT INTO `oracle`.`service_specific_answer` (`id`, `answer`, `question_id`, `user_id`) VALUES (5, 'FALSE', 5, 345);
+INSERT INTO `oracle`.`service_specific_answer` (`id`, `answer`, `question_id`, `user_id`) VALUES (6, 'TRUE', 6, 456);
+INSERT INTO `oracle`.`service_specific_answer` (`id`, `answer`, `question_id`, `user_id`) VALUES (7, 'A,B,C,D', 7, 567);
