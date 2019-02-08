4957bd29a3ab8b2ce07643c27b1598fd7579b7d5
Jiahao Cai <cai.jiah@husky.neu.edu>
2019-02-07 23:09:52 -0500

    add username for user567

--- src/main/resources/sql/insert_users.sql
+++ src/main/resources/sql/insert_users.sql
@@ -2,4 +2,4 @@ INSERT INTO users (id, username, password, first_name, last_name, role) VALUES (
 INSERT INTO users (id, username, password, first_name, last_name, role) VALUES (234, 'bob', 'bob', 'Bob', 'Marley', 'client');
 INSERT INTO users (id, username, password, first_name, last_name, role) VALUES (345, 'charlie', 'charlie', 'Charlie', 'Brown', 'provider');
 INSERT INTO users (id, first_name, last_name, password, role, username) VALUES (456, 'Michael', 'Jordan', 'goat', 'provider', 'jordan');
-INSERT INTO users (id, first_name, last_name, password, role) VALUES (567, 'Pokemon', 'Go', 'pokemon', 'provider');
+INSERT INTO users (id, first_name, last_name, password, role, username) VALUES (567, 'Pokemon', 'Go', 'pokemon', 'provider', 'pokemon');
