dc53f7e007f2252bc83948c4c2ee43ada708c819
Zerun <tian.ze@husky.neu.edu>
2019-02-13 22:14:29 -0500

    allowed crossorigin any

--- src/main/java/edu/neu/cs4500/services/ServiceSpecificQuestionService.java
+++ src/main/java/edu/neu/cs4500/services/ServiceSpecificQuestionService.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import edu.neu.cs4500.repositories.ServiceSpecificAnswerRepository;
 import edu.neu.cs4500.repositories.ServiceSpecificQuestionRepository;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ServiceSpecificQuestionService {
   @Autowired
   ServiceSpecificQuestionRepository serviceSpecificQuestionRepository;
