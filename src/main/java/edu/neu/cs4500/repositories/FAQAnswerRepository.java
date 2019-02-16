b5d1e53533352758be1807233a67195794d5ddbc
Jacob Hutchinson <jacobhutchinson@Jacobs-MBP.hsd1.ma.comcast.net>
2019-02-15 19:08:22 -0500

    Added JPA model for FAQs

---
+++ src/main/java/edu/neu/cs4500/repositories/FAQAnswerRepository.java
@@ -0,0 +1,18 @@
+package edu.neu.cs4500.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.example.cs4500sp19s2jga.models.FrequentlyAskedAnswer;
+
+public interface FAQAnswerRepository
+    extends CrudRepository<FrequentlyAskedAnswer, Integer> {
+    @Query(value="SELECT entity FROM FrequentlyAskedAnswer entity")
+    public List<FrequentlyAskedAnswer> findAllFrequentlyAskedAnswers();
+    @Query(value="SELECT entity FROM FrequentlyAskedAnswer entity WHERE id=:id")
+    public FrequentlyAskedAnswer findFrequentlyAskedAnswerById(
+            @Param("id") Integer id);
+}
