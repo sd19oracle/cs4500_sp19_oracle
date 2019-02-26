4af11b93e80d03cfc48354bcfc6c832dbfdfc6f4
Michael Goodnow <mmgoodnow@gmail.com>
2019-02-26 01:38:58 -0500

    Write first test for delivery fees

--- src/main/java/edu/neu/cs4500/models/Estimate.java
+++ src/main/java/edu/neu/cs4500/models/Estimate.java
@@ -3,55 +3,34 @@ package edu.neu.cs4500.models;
 import java.util.List;
 import java.util.stream.Collectors;
 
-//import javax.persistence.*;
-
-//@Entity
-//@Table(name="estimates")
 public class Estimate {
 
-    //@Id
-    //@GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Integer id;
-
-    private float estimate;
     private float basePrice;
-    private Frequency baseFrequency;
-    private boolean subscription;
     private Frequency subscriptionFrequency;
     private Frequency deliveryFrequency;
+	List<DeliveryFee> fees;
 
-    public Estimate(float estimate, float basePrice, Frequency baseFrequency,
-            boolean subscription, Frequency subscriptionFrequency, Frequency deliveryFrequency) {
-        this.estimate = estimate;
+    public Estimate(
+			float basePrice,
+			Frequency subscriptionFrequency,
+			Frequency deliveryFrequency,
+			List<DeliveryFee> fees) {
         this.basePrice = basePrice;
-        this.baseFrequency = baseFrequency;
-        this.subscription = subscription;
         this.subscriptionFrequency = subscriptionFrequency;
         this.deliveryFrequency = deliveryFrequency;
+        this.fees = fees;
     }
 
     public Estimate() {
-        
-    }
-            
-    public Integer getId() {
-        return id;
+
     }
 
-    public float getEstimate() {
-        return estimate;
+    public float getEstimate(List<DeliveryFee> fees, List<SubscriptionDiscount> discounts) {
+        return this.basePrice + this.getFees() - this.getDiscount(discounts);
     }
 
     public float getBasePrice() {
         return basePrice;
-    }
-
-    public Frequency getBaseFrequency() {
-        return baseFrequency;
-    }
-
-    public boolean getSubscription() {
-        return subscription;
     }
 
     public Frequency getSubscriptionFrequency() {
@@ -62,20 +41,8 @@ public class Estimate {
         return deliveryFrequency;
     }
 
-    public void setEstimate(float estimate) {
-        this.estimate = estimate;
-    }
-
     public void setBasePrice(float basePrice) {
         this.basePrice = basePrice;
-    }
-
-    public void setBaseFrequency(Frequency baseFrequency) {
-        this.baseFrequency = baseFrequency;
-    }
-
-    public void setSubscription(boolean subscription) {
-        this.subscription = subscription;
     }
 
     public void setSubscriptionFrequency(Frequency subscriptionFrequency) {
@@ -102,10 +69,10 @@ public class Estimate {
         return accumulateDiscount;
     }
 
-    public float getFees(List<DeliveryFee> fees) {
+    public float getFees() {
     	float acc = 0f;
 
-    	List<DeliveryFee> applicableFees = fees
+    	List<DeliveryFee> applicableFees = this.fees
 				.stream()
 				.filter(fee -> fee.getFrequency() == this.deliveryFrequency)
 				.collect(Collectors.toList());
---
+++ src/test/java/edu/neu/cs4500/models/DeliveryFeeEstimateTest.java
@@ -0,0 +1,44 @@
+package edu.neu.cs4500.models;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeliveryFeeEstimateTest {
+	Estimate estimate1;
+	Estimate estimate2;
+	Frequency daily = Frequency.DAILY;
+	Frequency weekday = Frequency.WEEKDAY;
+	Frequency monthly = Frequency.MONTHLY;
+	Frequency onetime = Frequency.ONETIME;
+
+	List<DeliveryFee> fees = new ArrayList<>();
+
+	DeliveryFee fee1 = new DeliveryFee(20, Frequency.ONETIME, true);
+	DeliveryFee fee2 = new DeliveryFee(0.1f, Frequency.MONTHLY, false);
+	DeliveryFee fee3 = new DeliveryFee(0.2f, Frequency.WEEKDAY, false);
+	DeliveryFee fee4 = new DeliveryFee(10, Frequency.WEEKDAY, true);
+
+
+	@BeforeEach
+	void setUp() {
+		this.estimate1 = new Estimate(100, this.daily, this.weekday, this.fees);
+		this.estimate2 = new Estimate(300, this.onetime, this.onetime, this.fees);
+		this.fees.clear();
+	}
+
+	@Test
+	void testFullComplexityFees() {
+		this.fees.add(this.fee1);
+		this.fees.add(this.fee2);
+		this.fees.add(this.fee3);
+		this.fees.add(this.fee4);
+		assertEquals(30, this.estimate1.getFees());
+		assertEquals(20, this.estimate2.getFees());
+	}
+}
