3c94548387471f8f152752072333cec645894976
Nianmin Guo <zenmai0822@live.com>
2019-04-01 00:24:43 -0400

    Added Icon and Popularity Field to Service Category, Added return by desc populatiry in corr. Repo

--- src/main/java/edu/neu/cs4500/repositories/ServiceCategoryRepository.java
+++ src/main/java/edu/neu/cs4500/repositories/ServiceCategoryRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.query.Param;
 import edu.neu.cs4500.models.ServiceCategory;
 
 public interface ServiceCategoryRepository extends CrudRepository<ServiceCategory, Integer> {
-	@Query(value="SELECT serviceCategory FROM ServiceCategory serviceCategory")
+	@Query(value="SELECT serviceCategory FROM ServiceCategory serviceCategory ORDER BY popularity DESC")
 	public List<ServiceCategory> findAllServiceCategories();
 	@Query(value="SELECT serviceCategory FROM ServiceCategory serviceCategory WHERE id=:servicecategoryid")
 	public ServiceCategory findServiceCategoryById(@Param("servicecategoryid") Integer id);
