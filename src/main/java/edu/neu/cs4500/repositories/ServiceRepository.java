71a63584705d76068d9b1c14269a16938f39a150
Jacob Hutchinson <hutchinson.j@husky.neu.edu>
2019-04-13 19:03:03 -0400

    Even more bug fixes

--- src/main/java/edu/neu/cs4500/repositories/ServiceRepository.java
+++ src/main/java/edu/neu/cs4500/repositories/ServiceRepository.java
@@ -3,6 +3,7 @@ package edu.neu.cs4500.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.PathVariable;
