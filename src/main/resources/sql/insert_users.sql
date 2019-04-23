20fdb10f6eea19e80e47c80b0d3f28e50bc39063
Michael Goodnow <mmgoodnow@gmail.com>
2019-04-22 21:39:17 -0400

    Fix bugs with users and emails

--- src/main/resources/sql/insert_users.sql
+++ src/main/resources/sql/insert_users.sql
@@ -1,8 +1,8 @@
-INSERT INTO users (id, username, password, first_name, last_name, role, zip_code) VALUES (123, 'alice', 'alice', 'Alice', 'Wonderland', 'admin', '02110');
-INSERT INTO users (id, username, password, first_name, last_name, role, zip_code) VALUES (234, 'bob', 'bob', 'Bob', 'Marley', 'client', '02115');
-INSERT INTO users (id, username, password, first_name, last_name, role, zip_code) VALUES (345, 'charlie', 'charlie', 'Charlie', 'Brown', 'provider', '02110');
-INSERT INTO users (id, first_name, last_name, password, role, username, zip_code) VALUES (456, 'Michael', 'Jordan', 'goat', 'provider', 'jordan', '10010');
-INSERT INTO users (id, first_name, last_name, password, role, username, zip_code) VALUES (567, 'Pokemon', 'Go', 'pokemon', 'provider', 'pokemon', '31256');
-INSERT INTO users (id, first_name, last_name, password, role, username, zip_code) VALUES (678, 'LeBron', 'James', 'goat', 'provider', 'James Cleaner', '02115');
-INSERT INTO users (id, first_name, last_name, password, role, username, zip_code) VALUES (789, 'Song', 'Park', 'itisfine', 'provider', 'Cleaner101', '02115');
-INSERT INTO users (id, first_name, last_name, password, role, username, zip_code) VALUES (890, 'Ana', 'Christina', 'ana', 'provider', 'Ana Cleaner', '02110');
+INSERT INTO users (id, email, password, first_name, last_name, role, zip_code) VALUES (123, 'alice', 'alice', 'Alice', 'Wonderland', 'admin', '02110');
+INSERT INTO users (id, email, password, first_name, last_name, role, zip_code) VALUES (234, 'bob', 'bob', 'Bob', 'Marley', 'client', '02115');
+INSERT INTO users (id, email, password, first_name, last_name, role, zip_code) VALUES (345, 'charlie', 'charlie', 'Charlie', 'Brown', 'provider', '02110');
+INSERT INTO users (id, first_name, last_name, password, role, email, zip_code) VALUES (456, 'Michael', 'Jordan', 'goat', 'provider', 'jordan', '10010');
+INSERT INTO users (id, first_name, last_name, password, role, email, zip_code) VALUES (567, 'Pokemon', 'Go', 'pokemon', 'provider', 'pokemon', '31256');
+INSERT INTO users (id, first_name, last_name, password, role, email, zip_code) VALUES (678, 'LeBron', 'James', 'goat', 'provider', 'James Cleaner', '02115');
+INSERT INTO users (id, first_name, last_name, password, role, email, zip_code) VALUES (789, 'Song', 'Park', 'itisfine', 'provider', 'Cleaner101', '02115');
+INSERT INTO users (id, first_name, last_name, password, role, email, zip_code) VALUES (890, 'Ana', 'Christina', 'ana', 'provider', 'Ana Cleaner', '02110');
