7a6002e62319a48fac235fc380c0a086ae145267
Zerun <tian.ze@husky.neu.edu>
2019-04-23 22:11:18 -0400

    add setters and getters

--- src/main/java/edu/neu/cs4500/models/User.java
+++ src/main/java/edu/neu/cs4500/models/User.java
@@ -35,6 +35,7 @@ public class User {
 
   @ManyToMany(mappedBy = "providers")
   private List<Service> services;
+
   public User() {
   }
 
@@ -109,7 +110,7 @@ public class User {
       this.answers.add(answer);
     }
   }
-  
+
   public List<Service> getServices() {
     return services;
   }
@@ -124,7 +125,39 @@ public class User {
 
   public void setZipCode(String zipCode) {
     this.zipCode = zipCode;
-  } 
+  }
+
+  public String getDob() {
+    return dob;
+  }
+
+  public void setDob(String dob) {
+    this.dob = dob;
+  }
+
+  public String getStreet() {
+    return street;
+  }
+
+  public void setStreet(String street) {
+    this.street = street;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public String getState() {
+    return state;
+  }
+
+  public void setState(String state) {
+    this.state = state;
+  }
 
   @Override
   public boolean equals(Object user) {
@@ -134,7 +167,7 @@ public class User {
     if (!(user instanceof User)) {
       return false;
     }
-    User tempUser = (User)user;
+    User tempUser = (User) user;
     return tempUser.getId().equals(this.id);
   }
 
